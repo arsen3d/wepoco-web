ce8b362334915e3f204d0858fe3d3f8f6ee16e67
mike.saunby <mike.saunby@70c02c81-bd25-0410-b0bc-110f2a487cf7>
2007-07-15 17:13:11 +0000

    This script creates tiles for Meteosat 7 at 57E.  Projection spec is improved over previous make_map_tiles.py so that should now be upgraded.

---
+++ trunk/mpe_tools/setup/make_map_tiles57.py
@@ -0,0 +1,200 @@
+#!/bin/env python
+# $Id: make_map_tiles.py 7 2006-12-30 20:10:37Z mike.saunby $
+#
+# Michael Saunby. For Wepoco.
+#
+# $Author: mike.saunby $
+# $Date: 2006-12-30 20:10:37 +0000 (Sat, 30 Dec 2006) $
+
+
+datadir='/home/mike/wepoco/data/mpe57/'
+mapdir = datadir + "map/"
+lutdir = datadir + "lut/"
+    
+import os, Image, ImageDraw, cbdmap, metcoords
+from gzip import GzipFile
+
+def do_draw(img,map,proj,flip=(False,True)):
+    (cols,rows) = img.size
+    last_col = cols - 1
+    last_row = rows - 1
+    (flipx,flipy) = flip
+    hdr = map.nexthdr()
+    draw = ImageDraw.Draw(img)
+    while hdr:
+        (segid, maxlat, minlat, maxlong, minlong,
+         rank, orglong, orglat, nstrokes) = hdr
+        miss = 0
+        try:
+            (checkx,checky)=proj.fwd((maxlong,maxlat))
+            if (checky < 1) or (checky > rows) or \
+                   (checkx < 1) or (checkx > cols):
+                miss += 1
+                pass
+            (checkx,checky)=proj.fwd((minlong,minlat))
+            if (checky < 1) or (checky > rows) or \
+                   (checkx < 1) or (checkx > cols):
+                miss += 1
+                pass
+            (checkx,checky)=proj.fwd((minlong,maxlat))
+            if (checky < 1) or (checky > rows) or \
+                   (checkx < 1) or (checkx > cols):
+                miss += 1
+                pass
+            (checkx,checky)=proj.fwd((maxlong,minlat))
+            if (checky < 1) or (checky > rows) or \
+                   (checkx < 1) or (checkx > cols):
+                miss += 1
+                pass
+            pass
+        except TypeError: # i.e. None returned by proj.fwd()
+            miss = 4
+            pass
+        if miss == 4:
+            hdr = map.nexthdr()
+            continue
+        (x,y) = proj.fwd((orglong,orglat))
+        if flipx:
+            x = last_col-x 
+            pass
+        if flipy:
+            y = last_row-y
+            pass
+        last_pt = (x,y)
+        pts = map.getpts()
+        for i in range(0,len(pts),2):
+            (x,y) = proj.fwd((pts[i],pts[i+1]))
+            if flipx:
+                x = last_col-x
+                pass
+            if flipy:
+                y = last_row-y
+                pass
+            draw.line(last_pt + (x,y), fill=0)
+            last_pt = (x,y)
+            pass
+        hdr = map.nexthdr()
+        pass
+    return
+
+def proj4_map(projfn, dims):
+    (width,height) = dims
+    img = Image.new('L', dims, color=255)
+    map = cbdmap.cbdfile(GzipFile("coast.bin.gz","rb"))
+    do_draw(img,map,projfn)
+    return img
+
+# Google Maps projection
+def google_proj(x, y, zoom, tile_size):
+    proj="merc"
+    #datum="WGS84"
+    lon_0=0.0
+    lat_0=0.0
+    to_meter= 156800 * pow(2,-zoom)
+
+    y_0 = tile_size * to_meter * ((y+1)-(2**(zoom-1))) 
+    x_0 = tile_size * to_meter * ((2**(zoom-1))-x)
+    
+    projfn = proj4.Projection(proj=proj,
+                              lat_0=lat_0,
+                              lon_0=lon_0,
+                              to_meter=to_meter,
+                              a=6378137,
+                              b=6378137,
+                              x_0=x_0, y_0=y_0)
+    return projfn
+    
+def map_tile(destdir, x, y, zoom):
+    tile_size = 256
+    projfn = google_proj(x, y, zoom, tile_size)
+    
+    outwidth = tile_size
+    outheight = tile_size
+    img = proj4_map(projfn,(outwidth, outheight))
+    tilename = "%d/%d_%d.png" % (zoom, x, y)
+    img.save( destdir + tilename, transparency=255 )
+
+def reproj_msat_lut(destdir, x, y, zoom):
+    import metcoords, array
+    tile_size = 256
+    projfn = google_proj(x, y, zoom, tile_size)
+    outwidth = tile_size
+    outheight = tile_size
+    met_fwd = metcoords.Meteosat(size=2500,generation=1,lon_0=57.0).fwd
+    met_rows = 2500
+    met_cols = 2500
+    matrix = array.array('h') # Short ints (half-words)
+    for row in range(outheight):
+        for col in range(outwidth):
+            lonlat = projfn.inv((col,(outheight-1)-row))
+            try:
+                (msatx,msaty) = met_fwd(lonlat)
+                matrix.append((met_cols-1)-int(msatx))
+                matrix.append((met_rows-1)-int(msaty))
+            except:
+                matrix.append(0)
+                matrix.append(0)
+            pass
+        pass
+    matrixname = "%d/%d_%d.msat_lut" % (zoom, x, y)
+    f = file(destdir + matrixname,'wb')
+    matrix.tofile(f)
+    f.close()
+
+def reproj_msg_lut(destdir, x, y, zoom):
+    import metcoords, array
+    tile_size = 256
+    projfn = google_proj(x, y, zoom, tile_size)
+    outwidth = tile_size
+    outheight = tile_size
+    met_fwd = metcoords.Meteosat(size=3712,generation=2).fwd
+    met_rows = 3712
+    met_cols = 3712
+    matrix = array.array('h') # Short ints (half-words)
+    for row in range(outheight):
+        for col in range(outwidth):
+            lonlat = projfn.inv((col,(outheight-1)-row))
+            try:
+                (msatx,msaty) = met_fwd(lonlat)
+                matrix.append((met_cols-1)-int(msatx))
+                matrix.append((met_rows-1)-int(msaty))
+            except:
+                matrix.append(0)
+                matrix.append(0)
+            pass
+        pass
+    matrixname = "%d/%d_%d.msg_lut" % (zoom, x, y)
+    f = file(destdir + matrixname,'wb')
+    matrix.tofile(f)
+    f.close()
+
+def prepare_tiles( zoom, x_min, y_min, x_max, y_max ):
+    try:
+        os.mkdir( mapdir + "%d" % (zoom) )
+        os.mkdir( lutdir + "%d" % (zoom) )
+    except:
+        print "dirs exist"
+        pass
+    for y in range(y_min,y_max):
+        for x in range(x_min,x_max):
+            print "generating map and lut for", x, y, zoom
+            map_tile( mapdir, x, y, zoom )
+            reproj_msat_lut( lutdir, x, y, zoom )
+            pass
+        pass
+    return
+    
+if __name__ == "__main__":
+    import proj4
+
+    try:
+        os.mkdir( mapdir )
+        os.mkdir( lutdir )
+    except:
+        print "dirs exist"
+        pass
+    
+    prepare_tiles(zoom=2, x_min=2, y_min=1, x_max=4, y_max=3 )
+#    prepare_tiles(zoom=3, x_min=5, y_min=2, x_max=7, y_max=5 )
+#    prepare_tiles(zoom=4, x_min=10, y_min=4, x_max=14, y_max=9 )
+#    prepare_tiles(zoom=5, x_min=20, y_min=8, x_max=28, y_max=17 )
