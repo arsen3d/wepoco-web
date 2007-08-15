e7a57e46247a850d580b5a80e04b5790e207f483
mike.saunby <mike.saunby@70c02c81-bd25-0410-b0bc-110f2a487cf7>
2007-08-15 19:52:32 +0000

    Wemapr  interactive google map layer creation for web2fordev conference.

---
+++ wemapr/ajax.js
@@ -0,0 +1,66 @@
+
+function makeHttpRequest(url, callback_function, return_xml)
+{
+    var http_request = false;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+        http_request = new XMLHttpRequest();
+        if (http_request.overrideMimeType) {
+            http_request.overrideMimeType('text/xml');
+        }
+    } else if (window.ActiveXObject) { // IE
+        try {
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {}
+        }
+    }
+
+    if (!http_request) {
+        alert('Unfortunatelly you browser doesn\'t support this feature.');
+        return false;
+    }
+    http_request.onreadystatechange = function() {
+        if (http_request.readyState == 4) {
+            if (http_request.status == 200) {
+                if (return_xml) {
+                    eval(callback_function + '(http_request.responseXML)');
+                } else {
+                    eval(callback_function + '(http_request.responseText)');
+                }
+            } else {
+                // alert('There was a problem with the request.(Code: ' + http_request.status + ')');
+            }
+        }
+    }
+    http_request.open('GET', url, true);
+    http_request.setRequestHeader( "If-Modified-Since", "Sat, 1 Jan 2000 00:00:00 GMT" );
+    http_request.send(null);
+}
+
+function _paramObj( name, value ){
+  this.name = name;
+  this.value = encodeURI( value );
+  this.getValue = function(){ return decodeURI(this.value); }
+}
+
+function ParamArray(){
+ this.store = new Array();
+ this.pushNameValue = function( name, value ){ 
+    this.store.push(new _paramObj(name, value)); 
+ } 
+ this.popURI = function(){ 
+    var text = "";
+    p=this.store.pop()
+    if(p){
+      text += "?" + p.name +"="+ p.value;
+    }
+    while( p=this.store.pop() ){
+      text += "&" + p.name +"="+ p.value;
+    }
+    return text;
+ }
+ return this;
+}
